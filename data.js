3e2fb235b2bd8c76411805726de37470ce49e505
lizhuoheng <lizhuohenglq@gmail.com>
2022-03-11 11:01:24 +0800

    1 file modified, 1 added

---
+++ data.js
@@ -0,0 +1,374 @@
+import G6 from '@antv/g6';
+
+/**
+ * format the string
+ * @param {string} str The origin string
+ * @param {number} maxWidth max width
+ * @param {number} fontSize font size
+ * @return {string} the processed result
+ */
+export const fittingString = (str, maxWidth, fontSize) => {
+  let currentWidth = 0;
+  let res = str;
+  const pattern = new RegExp('[\u4E00-\u9FA5]+'); // distinguish the Chinese charactors and letters
+  str.split('').forEach((letter, i) => {
+    if (currentWidth > maxWidth) return;
+    if (pattern.test(letter)) {
+      // Chinese charactors
+      currentWidth += fontSize;
+    } else {
+      // get the width of single letter according to the fontSize
+      currentWidth += G6.Util.getLetterWidth(letter, fontSize);
+    }
+    if (currentWidth > maxWidth) {
+      res = `${res.substr(0, i)}\n${res.substr(i)}`;
+      currentWidth = 0;
+    }
+  });
+  return res;
+};
+
+const globalFontSize = 11;
+
+export const data = {
+  nodes: [
+    {
+      id: '0',
+      label: fittingString('0【基础】集合的概念与性质', 65, globalFontSize),
+
+      comboId: 'A',
+    },
+    {
+      id: '1',
+      label: fittingString('1【基础】判断能否构成集合', 65, globalFontSize),
+      comboId: 'A',
+    },
+    {
+      id: '2',
+      label: fittingString(
+        '2【基础】集合相等的判断与相关计算',
+        65,
+        globalFontSize
+      ),
+      comboId: 'A',
+    },
+    {
+      id: '3',
+      label: fittingString('3判断元素与集合的关系', 65, globalFontSize),
+      comboId: 'B',
+    },
+    {
+      id: '4',
+      label: fittingString('4根据元素与集合的关系求参数', 65, globalFontSize),
+      comboId: 'B',
+    },
+    {
+      id: '5',
+      label: fittingString('5根据集合中元素的个数求参数', 65, globalFontSize),
+      comboId: 'B',
+    },
+    // {
+    //   id: '6',
+    //   label: fittingString('[废弃]利用元素的互异性求参数', 65, globalFontSize),
+    //   comboId: 'C',
+    // },
+    {
+      id: '7',
+      label: fittingString('7求集合元素的个数', 65, globalFontSize),
+      comboId: 'C',
+    },
+    {
+      id: '8',
+      label: fittingString('8集合元素互异性的应用', 65, globalFontSize),
+      comboId: 'C',
+    },
+    {
+      id: '9',
+      label: fittingString('9描述法表示集合', 65, globalFontSize),
+      comboId: 'D',
+    },
+    {
+      id: '10',
+      label: fittingString('10列举法表示集合', 65, globalFontSize),
+      comboId: 'D',
+    },
+    // {
+    //   id: '11',
+    //   label: fittingString('11判断子集的个数', 65, globalFontSize),
+    //   comboId: 'E',
+    // },
+    {
+      id: '12',
+      label: fittingString('12求集合的子集与子集个数', 65, globalFontSize),
+      comboId: 'E',
+    },
+    {
+      id: '13',
+      label: fittingString('13判断两个集合的包含关系', 65, globalFontSize),
+      comboId: 'E',
+    },
+    {
+      id: '14',
+      label: fittingString('14根据集合的包含关系求参数', 65, globalFontSize),
+      comboId: 'E',
+    },
+    {
+      id: '15',
+      label: fittingString('15集合相等与子集的关系', 65, globalFontSize),
+      comboId: 'E',
+    },
+    {
+      id: '16',
+      label: fittingString('16空集的概念与性质', 65, globalFontSize),
+    },
+    {
+      id: '17',
+      label: fittingString('17交集的概念及运算', 65, globalFontSize),
+      comboId: 'F',
+    },
+    {
+      id: '18',
+      label: fittingString('18根据交集结果逆向分析', 65, globalFontSize),
+      comboId: 'F',
+    },
+    {
+      id: '19',
+      label: fittingString('19并集的概念及运算', 65, globalFontSize),
+      comboId: 'F',
+    },
+    {
+      id: '20',
+      label: fittingString('20根据并集结果逆向分析', 65, globalFontSize),
+      comboId: 'F',
+    },
+    {
+      id: '21',
+      label: fittingString('21补集、全集的概念及运算', 65, globalFontSize),
+      comboId: 'F',
+    },
+    {
+      id: '22',
+      label: fittingString('22根据补集结果逆向分析', 65, globalFontSize),
+      comboId: 'F',
+    },
+    {
+      id: '23',
+      label: fittingString('23集合的交并补混合运算', 65, globalFontSize),
+      comboId: 'G',
+    },
+    {
+      id: '24',
+      label: fittingString(
+        '24根据交并补混合运算结果逆向分析',
+        65,
+        globalFontSize
+      ),
+      comboId: 'G',
+    },
+    {
+      id: '25',
+      label: fittingString('25Venn图的基本应用', 65, globalFontSize),
+      comboId: 'H',
+    },
+    {
+      id: '26',
+      label: fittingString('26容斥原理的应用', 65, globalFontSize),
+      comboId: 'H',
+    },
+    {
+      id: '28',
+      label: fittingString('27集合中的新定义问题', 65, globalFontSize),
+    },
+  ],
+  edges: [
+    {
+      source: '0',
+      target: '1',
+      label: '前驱',
+    },
+    {
+      source: '0',
+      target: '2',
+      label: '前驱',
+    },
+    {
+      source: '0',
+      target: '3',
+      label: '前驱',
+    },
+    {
+      source: '3',
+      target: '4',
+    },
+    {
+      source: '0',
+      target: '5',
+    },
+    {
+      source: '0',
+      target: '8',
+    },
+    {
+      source: '0',
+      target: '7',
+    },
+    {
+      source: '0',
+      target: '9',
+    },
+    {
+      source: '0',
+      target: '10',
+    },
+    {
+      source: '0',
+      target: '12',
+    },
+    {
+      source: '12',
+      target: '13',
+    },
+    {
+      source: '13',
+      target: '14',
+    },
+    {
+      source: '2',
+      target: '15',
+    },
+    {
+      source: '13',
+      target: '15',
+    },
+    {
+      source: '0',
+      target: '16',
+    },
+    {
+      source: '16',
+      target: '12',
+    },
+    {
+      source: '0',
+      target: '17',
+    },
+    {
+      source: '17',
+      target: '18',
+    },
+    {
+      source: '0',
+      target: '19',
+    },
+    {
+      source: '19',
+      target: '20',
+    },
+    {
+      source: '0',
+      target: '21',
+    },
+    {
+      source: '21',
+      target: '22',
+    },
+    {
+      source: '17',
+      target: '23',
+    },
+    {
+      source: '17',
+      target: '23',
+    },
+    {
+      source: '19',
+      target: '23',
+    },
+    {
+      source: '21',
+      target: '23',
+    },
+    {
+      source: '23',
+      target: '24',
+    },
+    {
+      source: '13',
+      target: '25',
+    },
+    {
+      source: '23',
+      target: '25',
+    },
+    {
+      source: '25',
+      target: '26',
+    },
+  ],
+  combos: [
+    {
+      id: 'A',
+      label: '集合的概念',
+      style: {
+        fill: '#C4E3B2',
+        stroke: '#C4E3B2',
+      },
+    },
+    {
+      id: 'B',
+      label: '元素与集合',
+      style: {
+        stroke: '#99C0ED',
+        fill: '#99C0ED',
+      },
+    },
+    {
+      id: 'C',
+      label: '集合性质的应用',
+      style: {
+        stroke: '#eee',
+        fill: '#eee',
+      },
+    },
+    {
+      id: 'D',
+      label: '集合的表示方法',
+      style: {
+        stroke: '#C4E3B2',
+        fill: '#C4E3B2',
+      },
+    },
+    {
+      id: 'E',
+      label: '集合间的基本关系',
+      style: {
+        stroke: '#99C0ED',
+        fill: '#99C0ED',
+      },
+    },
+    {
+      id: 'F',
+      label: '集合的基本运算',
+      style: {
+        stroke: '#eee',
+        fill: '#eee',
+      },
+    },
+    {
+      id: 'G',
+      label: '集合的交并补混合运算',
+      style: {
+        stroke: '#C4E3B2',
+        fill: '#C4E3B2',
+      },
+    },
+    {
+      id: 'H',
+      label: 'Venn图',
+      style: {
+        stroke: '#99C0ED',
+        fill: '#99C0ED',
+      },
+    },
+  ],
+};
--- index.js
+++ index.js
@@ -1,406 +1,264 @@
 import G6 from '@antv/g6';
-import { GraphLayoutPredict } from '@antv/vis-predict-engine';
+import { data } from './data';
+// import { GraphLayoutPredict } from '@antv/vis-predict-engine';
 
-/**
- * format the string
- * @param {string} str The origin string
- * @param {number} maxWidth max width
- * @param {number} fontSize font size
- * @return {string} the processed result
- */
-const fittingString = (str, maxWidth, fontSize) => {
-  let currentWidth = 0;
-  let res = str;
-  const pattern = new RegExp('[\u4E00-\u9FA5]+'); // distinguish the Chinese charactors and letters
-  str.split('').forEach((letter, i) => {
-    if (currentWidth > maxWidth) return;
-    if (pattern.test(letter)) {
-      // Chinese charactors
-      currentWidth += fontSize;
-    } else {
-      // get the width of single letter according to the fontSize
-      currentWidth += G6.Util.getLetterWidth(letter, fontSize);
+// data.nodes.forEach((node) => {
+//   switch (node.comboId) {
+//     case 'A':
+//       node.style = {
+//         fill: '#C4E3B2',
+//         stroke: '#aaa',
+//       };
+//       break;
+//     case 'B':
+//       node.style = {
+//         fill: '#99C0ED',
+//         stroke: '#aaa',
+//       };
+//       break;
+//     case 'C':
+//       node.style = {
+//         fill: '#eee',
+//         stroke: '#aaa',
+//       };
+//       break;
+//     default:
+//       node.style = {
+//         fill: '#FDE1CE',
+//         stroke: '#aaa',
+//       };
+//       break;
+//   }
+// });
+
+G6.registerBehavior('activate-knode', {
+  getDefaultCfg() {
+    return {
+      // 可选 mouseenter || click
+      // 选择 click 会监听 touch，mouseenter 不会监听
+      trigger: 'click',
+      activeState: 'selected', //active
+      // inactiveState: 'inactive',
+      resetSelected: false,
+      shouldUpdate() {
+        return true;
+      },
+    };
+  },
+  getEvents() {
+    if (this.get('trigger') === 'mouseenter') {
+      return {
+        'node:mouseenter': 'setAllItemStates',
+        'combo:mouseenter': 'setAllItemStates',
+        'node:mouseleave': 'clearActiveState',
+        'combo:mouseleave': 'clearActiveState',
+      };
     }
-    if (currentWidth > maxWidth) {
-      res = `${res.substr(0, i)}\n${res.substr(i)}`;
-      currentWidth = 0;
+    return {
+      'node:click': 'setAllItemStates',
+      'combo:click': 'setAllItemStates',
+      'canvas:click': 'clearActiveState',
+      'node:touchstart': 'setOnTouchStart',
+      'combo:touchstart': 'setOnTouchStart',
+      // 'canvas:touchend': 'clearOnTouchStart',
+    };
+  },
+
+  setOnTouchStart(e) {
+    const self = this;
+    try {
+      const touches = e.originalEvent.touches;
+      const event1 = touches[0];
+      const event2 = touches[1];
+
+      if (event1 && event2) {
+        return;
+      }
+
+      e.preventDefault();
+    } catch (e) {
+      console.warn('Touch original event not exist!');
     }
-  });
-  return res;
-};
+    self.setAllItemStates(e);
+  },
+  clearOnTouchStart(e) {
+    const self = this;
+    try {
+      const touches = e.originalEvent.touches;
+      const event1 = touches[0];
+      const event2 = touches[1];
 
-const globalFontSize = 11;
+      if (event1 && event2) {
+        return;
+      }
 
-const data = {
-  nodes: [
-    {
-      id: '0',
-      label: fittingString('0【基础】集合的概念与性质', 65, globalFontSize),
+      e.preventDefault();
+    } catch (e) {
+      console.warn('Touch original event not exist!');
+    }
+    self.clearActiveState(e);
+  },
+  setAllItemStates(e) {
+    const item = e.item;
+    const graph = this.graph;
+    this.item = item;
+    if (!this.shouldUpdate(e.item, { event: e, action: 'activate' })) {
+      return;
+    }
+    const self = this;
+    const activeState = this.activeState;
+    const inactiveState = this.inactiveState;
+    const nodes = graph.getNodes();
+    const combos = graph.getCombos();
+    const edges = graph.getEdges();
+    const vEdges = graph.get('vedges');
+    const nodeLength = nodes.length;
+    const comboLength = combos.length;
+    const edgeLength = edges.length;
+    const vEdgeLength = vEdges.length;
 
-      comboId: 'A',
-    },
-    {
-      id: '1',
-      label: fittingString('1【基础】判断能否构成集合', 65, globalFontSize),
-      comboId: 'A',
-    },
-    {
-      id: '2',
-      label: fittingString(
-        '2【基础】集合相等的判断与相关计算',
-        65,
-        globalFontSize
-      ),
-      comboId: 'A',
-    },
-    {
-      id: '3',
-      label: fittingString('3判断元素与集合的关系', 65, globalFontSize),
-      comboId: 'B',
-    },
-    {
-      id: '4',
-      label: fittingString('4根据元素与集合的关系求参数', 65, globalFontSize),
-      comboId: 'B',
-    },
-    {
-      id: '5',
-      label: fittingString('5根据集合中元素的个数求参数', 65, globalFontSize),
-      comboId: 'B',
-    },
-    // {
-    //   id: '6',
-    //   label: fittingString('[废弃]利用元素的互异性求参数', 65, globalFontSize),
-    //   comboId: 'C',
-    // },
-    {
-      id: '7',
-      label: fittingString('7求集合元素的个数', 65, globalFontSize),
-      comboId: 'C',
-    },
-    {
-      id: '8',
-      label: fittingString('8集合元素互异性的应用', 65, globalFontSize),
-      comboId: 'C',
-    },
-    {
-      id: '9',
-      label: fittingString('9描述法表示集合', 65, globalFontSize),
-      comboId: 'D',
-    },
-    {
-      id: '10',
-      label: fittingString('10列举法表示集合', 65, globalFontSize),
-      comboId: 'D',
-    },
-    // {
-    //   id: '11',
-    //   label: fittingString('11判断子集的个数', 65, globalFontSize),
-    //   comboId: 'E',
-    // },
-    {
-      id: '12',
-      label: fittingString('12求集合的子集与子集个数', 65, globalFontSize),
-      comboId: 'E',
-    },
-    {
-      id: '13',
-      label: fittingString('13判断两个集合的包含关系', 65, globalFontSize),
-      comboId: 'E',
-    },
-    {
-      id: '14',
-      label: fittingString('14根据集合的包含关系求参数', 65, globalFontSize),
-      comboId: 'E',
-    },
-    {
-      id: '15',
-      label: fittingString('15集合相等与子集的关系', 65, globalFontSize),
-      comboId: 'E',
-    },
-    {
-      id: '16',
-      label: fittingString('16空集的概念与性质', 65, globalFontSize),
-    },
-    {
-      id: '17',
-      label: fittingString('17交集的概念及运算', 65, globalFontSize),
-      comboId: 'F',
-    },
-    {
-      id: '18',
-      label: fittingString('18根据交集结果逆向分析', 65, globalFontSize),
-      comboId: 'F',
-    },
-    {
-      id: '19',
-      label: fittingString('19并集的概念及运算', 65, globalFontSize),
-      comboId: 'F',
-    },
-    {
-      id: '20',
-      label: fittingString('20根据并集结果逆向分析', 65, globalFontSize),
-      comboId: 'F',
-    },
-    {
-      id: '21',
-      label: fittingString('21补集、全集的概念及运算', 65, globalFontSize),
-      comboId: 'F',
-    },
-    {
-      id: '22',
-      label: fittingString('22根据补集结果逆向分析', 65, globalFontSize),
-      comboId: 'F',
-    },
-    {
-      id: '23',
-      label: fittingString('23集合的交并补混合运算', 65, globalFontSize),
-      comboId: 'G',
-    },
-    {
-      id: '24',
-      label: fittingString(
-        '24根据交并补混合运算结果逆向分析',
-        65,
-        globalFontSize
-      ),
-      comboId: 'G',
-    },
-    {
-      id: '25',
-      label: fittingString('25Venn图的基本应用', 65, globalFontSize),
-      comboId: 'H',
-    },
-    {
-      id: '26',
-      label: fittingString('26容斥原理的应用', 65, globalFontSize),
-      comboId: 'H',
-    },
-    {
-      id: '28',
-      label: fittingString('27集合中的新定义问题', 65, globalFontSize),
-    },
-  ],
-  edges: [
-    {
-      source: '0',
-      target: '1',
-      label: '前驱',
-    },
-    {
-      source: '0',
-      target: '2',
-      label: '前驱',
-    },
-    {
-      source: '0',
-      target: '3',
-      label: '前驱',
-    },
-    {
-      source: '3',
-      target: '4',
-    },
-    {
-      source: '0',
-      target: '5',
-    },
-    {
-      source: '0',
-      target: '8',
-    },
-    {
-      source: '0',
-      target: '7',
-    },
-    {
-      source: '0',
-      target: '9',
-    },
-    {
-      source: '0',
-      target: '10',
-    },
-    {
-      source: '0',
-      target: '12',
-    },
-    {
-      source: '12',
-      target: '13',
-    },
-    {
-      source: '13',
-      target: '14',
-    },
-    {
-      source: '2',
-      target: '15',
-    },
-    {
-      source: '13',
-      target: '15',
-    },
-    {
-      source: '0',
-      target: '16',
-    },
-    {
-      source: '16',
-      target: '12',
-    },
-    {
-      source: '0',
-      target: '17',
-    },
-    {
-      source: '17',
-      target: '18',
-    },
-    {
-      source: '0',
-      target: '19',
-    },
-    {
-      source: '19',
-      target: '20',
-    },
-    {
-      source: '0',
-      target: '21',
-    },
-    {
-      source: '21',
-      target: '22',
-    },
-    {
-      source: '17',
-      target: '23',
-    },
-    {
-      source: '17',
-      target: '23',
-    },
-    {
-      source: '19',
-      target: '23',
-    },
-    {
-      source: '21',
-      target: '23',
-    },
-    {
-      source: '23',
-      target: '24',
-    },
-    {
-      source: '13',
-      target: '25',
-    },
-    {
-      source: '23',
-      target: '25',
-    },
-    {
-      source: '25',
-      target: '26',
-    },
-  ],
-  //   combos: [
-  //     {
-  //       id: 'A',
-  //       label: '集合的概念',
-  //       style: {
-  //         fill: '#C4E3B2',
-  //         stroke: '#C4E3B2',
-  //       },
-  //     },
-  //     {
-  //       id: 'B',
-  //       label: '元素与集合',
-  //       style: {
-  //         stroke: '#99C0ED',
-  //         fill: '#99C0ED',
-  //       },
-  //     },
-  //     {
-  //       id: 'C',
-  //       label: '集合性质的应用',
-  //       style: {
-  //         stroke: '#eee',
-  //         fill: '#eee',
-  //       },
-  //     },
-  //     {
-  //       id: 'D',
-  //       label: '集合的表示方法',
-  //       style: {
-  //         stroke: '#C4E3B2',
-  //         fill: '#C4E3B2',
-  //       },
-  //     },
-  //     {
-  //       id: 'E',
-  //       label: '集合间的基本关系',
-  //       style: {
-  //         stroke: '#99C0ED',
-  //         fill: '#99C0ED',
-  //       },
-  //     },
-  //     {
-  //       id: 'F',
-  //       label: '集合的基本运算',
-  //       style: {
-  //         stroke: '#eee',
-  //         fill: '#eee',
-  //       },
-  //     },
-  //     {
-  //       id: 'G',
-  //       label: '集合的交并补混合运算',
-  //       style: {
-  //         stroke: '#C4E3B2',
-  //         fill: '#C4E3B2',
-  //       },
-  //     },
-  //     {
-  //       id: 'H',
-  //       label: 'Venn图',
-  //       style: {
-  //         stroke: '#99C0ED',
-  //         fill: '#99C0ED',
-  //       },
-  //     },
-  //   ],
-};
+    for (let i = 0; i < nodeLength; i++) {
+      const node = nodes[i];
+      const hasSelected = node.hasState('selected');
+      if (self.resetSelected) {
+        if (hasSelected) {
+          graph.setItemState(node, 'selected', false);
+        }
+      }
+      graph.setItemState(node, activeState, false);
+      if (inactiveState) {
+        graph.setItemState(node, inactiveState, true);
+      }
+    }
+    for (let i = 0; i < comboLength; i++) {
+      const combo = combos[i];
+      const hasSelected = combo.hasState('selected');
+      if (self.resetSelected) {
+        if (hasSelected) {
+          graph.setItemState(combo, 'selected', false);
+        }
+      }
+      graph.setItemState(combo, activeState, false);
+      if (inactiveState) {
+        graph.setItemState(combo, inactiveState, true);
+      }
+    }
 
-data.nodes.forEach((node) => {
-  switch (node.comboId) {
-    case 'A':
-      node.style = {
-        fill: '#C4E3B2',
-        stroke: '#aaa',
-      };
-      break;
-    case 'B':
-      node.style = {
-        fill: '#99C0ED',
-        stroke: '#aaa',
-      };
-      break;
-    case 'C':
-      node.style = {
-        fill: '#eee',
-        stroke: '#aaa',
-      };
-      break;
-    default:
-      node.style = {
-        fill: '#FDE1CE',
-        stroke: '#aaa',
-      };
-      break;
-  }
+    for (let i = 0; i < edgeLength; i++) {
+      const edge = edges[i];
+      graph.setItemState(edge, activeState, false);
+      if (inactiveState) {
+        graph.setItemState(edge, inactiveState, true);
+      }
+    }
+
+    for (let i = 0; i < vEdgeLength; i++) {
+      const vEdge = vEdges[i];
+      graph.setItemState(vEdge, activeState, false);
+      if (inactiveState) {
+        graph.setItemState(vEdge, inactiveState, true);
+      }
+    }
+
+    if (inactiveState) {
+      graph.setItemState(item, inactiveState, false);
+    }
+    graph.setItemState(item, activeState, true);
+
+    const rEdges = item.getEdges();
+    const rEdgeLegnth = rEdges.length;
+    for (let i = 0; i < rEdgeLegnth; i++) {
+      const edge = rEdges[i];
+      let otherEnd;
+      if (edge.getSource() === item) {
+        otherEnd = edge.getTarget();
+      } else {
+        otherEnd = edge.getSource();
+      }
+      if (inactiveState) {
+        graph.setItemState(otherEnd, inactiveState, false);
+      }
+      graph.setItemState(otherEnd, activeState, true);
+      graph.setItemState(edge, inactiveState, false);
+      graph.setItemState(edge, activeState, true);
+      edge.toFront();
+    }
+    graph.emit('afteractivaterelations', { item: e.item, action: 'activate' });
+  },
+  clearActiveState(e) {
+    const self = this;
+    const graph = self.get('graph');
+    if (!self.shouldUpdate(e.item, { event: e, action: 'deactivate' })) {
+      return;
+    }
+
+    const activeState = this.activeState;
+    const inactiveState = this.inactiveState;
+
+    const autoPaint = graph.get('autoPaint');
+    graph.setAutoPaint(false);
+    const nodes = graph.getNodes() || [];
+    const combos = graph.getCombos() || [];
+    const edges = graph.getEdges() || [];
+    const vEdges = graph.get('vedges') || [];
+    const nodeLength = nodes.length;
+    const comboLength = combos.length;
+    const edgeLength = edges.length;
+    const vEdgeLength = vEdges.length;
+
+    for (let i = 0; i < nodeLength; i++) {
+      const node = nodes[i];
+      graph.clearItemStates(node, [activeState, inactiveState]);
+    }
+    for (let i = 0; i < comboLength; i++) {
+      const combo = combos[i];
+      graph.clearItemStates(combo, [activeState, inactiveState]);
+    }
+    for (let i = 0; i < edgeLength; i++) {
+      const edge = edges[i];
+      graph.clearItemStates(edge, [activeState, inactiveState, 'deactivate']);
+    }
+    for (let i = 0; i < vEdgeLength; i++) {
+      const vEdge = vEdges[i];
+      graph.clearItemStates(vEdge, [activeState, inactiveState, 'deactivate']);
+    }
+    graph.paint();
+    graph.setAutoPaint(autoPaint);
+    graph.emit('afteractivaterelations', {
+      item: e.item || self.get('item'),
+      action: 'deactivate',
+    });
+  },
+
+  // onNodeClick(e) {
+  //   console.log('---------evt;');
+  //   const graph = this.graph;
+  //   const item = e.item;
+  //   if (item.hasState(this.selectedState)) {
+  //     graph.setItemState(item, this.selectedState, false);
+  //     return;
+  //   }
+  //   // this 上即可取到配置，如果不允许多个 'active'，先取消其他节点的 'active' 状态
+  //   if (!this.multiple) {
+  //     this.removeNodesState();
+  //   }
+  //   // 置点击的节点状态 'active' 为 true
+  //   graph.setItemState(item, this.selectedState, true);
+  // },
+  // onCanvasClick(e) {
+  //   // shouldUpdate 可以由用户复写，返回 true 时取消所有节点的 'active' 状态，即将 'active' 状态置为 false
+  //   if (this.shouldUpdate(e)) {
+  //     this.removeNodesState();
+  //   }
+  // },
+  // removeNodesState() {
+  //   graph.findAllByState('node', this.selectedState).forEach((node) => {
+  //     graph.setItemState(node, this.selectedState, false);
+  //   });
+  //   graph.findAllByState('combo', this.selectedState).forEach((combo) => {
+  //     graph.setItemState(combo, this.selectedState, false);
+  //   });
+  // },
 });
 
 const container = document.getElementById('container');
@@ -419,17 +277,30 @@ const graph = new G6.Graph({
   fitViewPadding: 30,
   modes: {
     default: [
-      'drag-combo',
+      // 'drag-combo',
+      // 'drag-node',
       'drag-canvas',
-      'drag-node',
-      {
-        type: 'collapse-expand-combo',
-        relayout: false,
-      },
+      'zoom-canvas',
+      // 'click-select',
+      'activate-knode',
+      // {
+      //   type: 'collapse-expand-combo',
+      //   trigger: 'dbclick', //click dbclick
+      //   relayout: false,
+      // },
+      // {
+      //   type: 'activate-relations',
+      //   trigger: 'click',
+      //   resetSelected: true,
+      // },
     ],
   },
   layout: {
     type: 'circular', //circular  dagre  concentric
+    startRadius: 250,
+    endRadius: 500,
+    angleRatio: 1,
+    // divisions:3,
     //rankdir: 'LR',
     //align: 'UL',
     sortByCombo: true,
@@ -438,18 +309,18 @@ const graph = new G6.Graph({
     ranksepFunc: () => 1,
   },
   defaultNode: {
-    size: 10,
+    size: 15,
     style: {
-      lineWidth: 2,
+      lineWidth: 1,
       stroke: '#5B8FF9',
       fill: '#C6E5FF',
     },
     labelCfg: {
       style: {
-        //fill: '#9ec9ff',
-        fontSize: 3,
+        fill: '#9ec9ff',
+        fontSize: 10,
         background: {
-          //fill: '#ffffff',
+          // fill: '#ffffff',
           stroke: '#9EC9FF',
           padding: [2, 2, 2, 2],
           radius: 2,
@@ -460,22 +331,24 @@ const graph = new G6.Graph({
   },
   defaultEdge: {
     //type: 'arc', //polyline
-    size: 1,
-    color: '#e2e2e2',
+    size: 2,
+    color: '#f5f5f5',
     style: {
       endArrow: {
-        path: 'M 0,0 L 8,2 L 8,-2 Z',
-        fill: '#e2e2e2',
+        path: 'M 0,0 L 8,3 L 8,-3 Z',
+        fill: '#f5f5f5',
+        // path: G6.Arrow.vee(10, -20, 10), // 内置箭头，参数为箭头宽度、长度、偏移量 d（默认为 0）
+        // d: 10 // 偏移量
       },
-      radius: 5,
+      // radius: 5,
     },
     labelCfg: {
       style: {
-        //fill: '#9ec9ff',
-        fontSize: 5,
+        fill: '#9ec9ff',
+        fontSize: 8,
         background: {
           fill: '#f6bd16',
-          //stroke: '#9EC9FF',
+          stroke: '#9EC9FF',
           padding: [2, 2, 2, 2],
           radius: 2,
         },
@@ -484,7 +357,7 @@ const graph = new G6.Graph({
     },
   },
   defaultCombo: {
-    type: 'rect',
+    type: 'circle', // rect  circle
     style: {
       fillOpacity: 0.1,
     },
@@ -492,6 +365,22 @@ const graph = new G6.Graph({
 });
 graph.data(data);
 graph.render();
+
+// graph.on('node:click', (evt) => {
+//   // clearFocusItemState(graph);
+//   // clearFocusEdgeState(graph);
+//   const { item } = evt;
+
+//   // highlight the clicked node, it is down by click-select
+//   graph.setItemState(item, 'focus', true);
+
+//     // 将相关边也高亮
+//     const relatedEdges = item.getEdges();
+//     relatedEdges.forEach((edge) => {
+//       graph.setItemState(edge, 'focus', true);
+//     });
+
+// });
 
 if (typeof window !== 'undefined')
   window.onresize = () => {
